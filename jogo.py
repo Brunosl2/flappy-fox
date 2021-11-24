d6e2229a65fe43c777925a004cf4eb6b74c1cd02
Bruno <brunosl2@al.insper.edu.br>
2021-11-24 16:39:24 -0300

    chao

--- jogo.py
+++ jogo.py
@@ -7,6 +7,10 @@ LARGURA = 400
 ALTURA = 600
 VELOCIDADE = 10
 GRAVIDADE = 1
+VEL_JOGO = 10 
+
+LARGURA_CHAO = 2 * LARGURA
+ALTURA_CHAO = 100
 
 #criando classes para definir imagens e movimentos
 class Raposa(pygame.sprite.Sprite):
@@ -31,7 +35,24 @@ class Raposa(pygame.sprite.Sprite):
 
     def pulo(self):
         self.speed = - VELOCIDADE
-        
+
+class Chao(pygame.sprite.Sprite):
+    
+    def __init__(self, x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('base.png')
+        self.image = pygame.transform.scale(self.image, (LARGURA_CHAO, ALTURA_CHAO))
+
+        self.rect = self.image.get_rect()
+        self.rect[1] = ALTURA - ALTURA_CHAO
+        self.rect[0] = x
+
+    def update(self):
+
+        self.rect[0] -= VEL_JOGO
+
+def fora_da_tela(sprite):
+    return sprite.rect[0] < -(sprite.rect[2])
 
 
 #inicia o jogo e configurações iniciais
@@ -48,8 +69,16 @@ raposa_group = pygame.sprite.Group()
 raposa = Raposa()
 raposa_group.add(raposa)
 
+chao_group = pygame.sprite.Group()
+
+for i in range(2):
+    chao = Chao(LARGURA_CHAO * i)
+    chao_group.add(chao)
+
+
 #dá um nome mais fácil para colocar o fps
 clock = pygame.time.Clock()
+
 
 
 while True:
@@ -65,9 +94,16 @@ while True:
     #mostra o fundo
     tela.blit(BACKGROUND, (0,0))
 
+    if fora_da_tela(chao_group.sprites()[0]):
+        chao_group.remove(chao_group.sprites()[0])
+
+        novo_chao = Chao(LARGURA_CHAO - 20)
+        chao_group.add(novo_chao)
+
     raposa_group.update()
+    chao_group.update()
     
     raposa_group.draw(tela)
-
+    chao_group.draw(tela)
     #atualiza a tela
     pygame.display.update()
